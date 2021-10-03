06663fc517aff1b55d240e2d617d6ebe4e55c251
Nischal Shrestha <nischalstha9@gmail.com>
2021-10-03 22:33:40 +0545

    Exception handling and method invoke qns no 6

--- methodInvoke/MethodInvoke.java
+++ methodInvoke/MethodInvoke.java
@@ -3,23 +3,23 @@ package methodInvoke;
 public class MethodInvoke {
   static void method1() {
     System.out.println("This is method 1");
-    method2();
-  }
-
-  static void method2() {
-    System.out.println("This is method 2");
     try {
-      method3();
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.getMessage();
-    }
+    	method2();	
+	}catch (Exception e){
+		System.out.println(e.getMessage());
+	}finally {
+		System.out.println("Program finished");
+	}
   }
 
-  static void method3() {
-    Exception exc = new Exception("ERROR");
+  static void method2() throws Exception {
+    System.out.println("This is method 2");
+    method3();
+  }
+
+  static void method3() throws Exception {
+    Exception exc = new Exception("This is error thrown from method3");
     throw exc;
-    // System.out.println("This is method 3");
   }
 
   public static void main(String[] args) {
