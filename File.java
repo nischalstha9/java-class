b64eab0f68bc765953c1c2b3f74327ac3f3499d3
Nischal Shrestha <nischalstha9@gmail.com>
2021-10-04 16:02:45 +0545

    File read/write

--- File.java
+++ File.java
@@ -1,3 +1,27 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 public class File {
-
+	public static void main(String[] args) {
+		FileInputStream readFile = null;
+		FileOutputStream writeFile = null;
+		String s = "";
+		try {
+			readFile = new FileInputStream("C:/Users/sthan/Desktop/java.txt");
+			writeFile = new FileOutputStream("C:/Users/sthan/Desktop/new_java.txt");
+			int b;
+			System.out.print("Text read from file: ");
+			while((b=readFile.read())!=-1) {
+				System.out.print((char) b);
+				s+=(char) b;
+			}
+			System.out.println("");
+			readFile.close();
+			byte writeByte[] = s.getBytes();
+			writeFile.write(writeByte);
+			System.out.println("Data successfully written to new file.");
+			writeFile.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
